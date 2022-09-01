c6bd6656dfc1abfe2a06ce543bd6f07f0ca07fa7
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-09-01 19:24:23 +0530

    Add links header

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ import getData from './getFoodList.js';
 // api links
 const allBeefURL = 'https:///www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
 const urlDetails = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+const allPastaURL = `
+  https://www.themealdb.com/api/json/v1/1/filter.php?c=pasta`;
+const allSeafoodURL = `
+  https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`;
+const [liBeefElement, liPastaElement, liSeafoodElement] =
+  document.querySelectorAll('li');
+let typeFoodSelected = liBeefElement;
+
+liBeefElement.addEventListener('click', () => {
+  
+  getData(allBeefURL);
+});
+
+liPastaElement.addEventListener('click', () => {
+  
+  getData(allPastaURL);
+});
+
+liSeafoodElement.addEventListener('click', () => {
+  
+  getData(allBeefURL);
+});
+
+
+
+
+
 getData(allBeefURL);
 
 export default allBeefURL;
