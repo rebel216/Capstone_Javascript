64d68a392c79ea68538a6c6388299b89359ea7df
AlucardSanin <alucardsanin@gmail.com>
2022-09-02 22:09:29 -0400

    Line Breaks LF

--- src/__test__/getlikes.test.js
+++ src/__test__/getlikes.test.js
@@ -4,7 +4,7 @@ global.fetch = jest.fn(() => Promise.resolve({
   json: () => Promise.resolve({ likes: 2 }),
 }));
 
-it('Should be show Beef (42) ', async () => {
+it('likes of the item should be 2 ', async () => {
   const data = await getlikes('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/oHdQExR6DIJGa8S6fY1E/likes/');
   expect(data).toEqual(2);
 });
